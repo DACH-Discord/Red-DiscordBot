428564773493090df125e98523af4f2c2d7b0236
Dama <dam4rusxp@googlemail.com>
2018-05-09 15:47:54 +0200

    Add lockdown module

---
+++ cogs/saftladen.py
@@ -0,0 +1,112 @@
+from discord import Channel, Role
+from discord.ext import commands
+from discord.utils import find
+
+from cogs.utils import checks
+
+
+class Saftladen:
+    lockdown_overwrites = {
+        "send_messages": False,
+        "add_reactions": False,
+        "speak": False
+    }
+
+    exception_overwrites = {
+        "send_messages": True,
+        "add_reactions": True,
+        "speak": True
+    }
+
+    exception_roles = [
+        "Mods",
+        "Bots"
+    ]
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.old_perms = dict()
+
+    async def overwrite(self, channel: Channel, role: Role, **overwrites):
+        if channel not in self.old_perms:
+            self.old_perms[channel] = dict()
+
+        if role not in self.old_perms[channel]:
+            self.old_perms[channel][role] = channel.overwrites_for(role)
+
+        new_ow = channel.overwrites_for(role)
+        new_ow.update(**overwrites)
+
+        await self.bot.edit_channel_permissions(channel, role, new_ow)
+
+    async def restore(self, channel: Channel) -> bool:
+        if channel in self.old_perms:
+            for role, old_ow in self.old_perms[channel].items():
+                if not old_ow.is_empty():
+                    await self.bot.edit_channel_permissions(channel, role, old_ow)
+                else:
+                    await self.bot.delete_channel_permissions(channel, role)
+
+            return True
+        else:
+            return False
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.mod()
+    async def lockdown(self, ctx, channel: Channel = None):
+        if not channel:
+            channel = ctx.message.channel
+        server = channel.server
+
+        # Lock out @everyone
+        await self.overwrite(channel, server.default_role, **self.lockdown_overwrites)
+
+        # Create exceptions
+        for role_name in self.exception_roles:
+            role = find(lambda r: r.name == role_name, server.roles)
+            await self.overwrite(channel, role, **self.exception_overwrites)
+
+        await self.bot.say("Lockdown engaged")
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.mod()
+    async def unlock(self, ctx, channel: Channel = None):
+        if not channel:
+            channel = ctx.message.channel
+
+        if await self.restore(channel):
+            await self.bot.say("All clear")
+        else:
+            await self.bot.say("Channel is not locked")
+
+    @commands.command(pass_context=True, no_pm=True, hidden=True)
+    @checks.mod()
+    async def showperms(self, ctx, channel: Channel = None):
+        if not channel:
+            channel = ctx.message.channel
+
+        msg = "```\n"
+        for target, overwrites in channel.overwrites:
+            msg += "%s: %s\n" % (target, overwrites._values)
+        msg += "```"
+
+        await self.bot.say(msg)
+
+    @commands.command(pass_context=True, no_pm=True, hidden=True)
+    @checks.admin()
+    async def clearperms(self, ctx, channel: Channel = None):
+        if not channel:
+            channel = ctx.message.channel
+
+        for target, overwrites in channel.overwrites:
+            await self.bot.delete_channel_permissions(channel, target)
+
+        if channel in self.old_perms:
+            del self.old_perms[channel]
+
+        await self.bot.say("Ok")
+
+
+def setup(bot):
+    n = Saftladen(bot)
+    bot.add_cog(n)
